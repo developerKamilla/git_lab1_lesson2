4d49d457ba96e497874a9c17c11017f9a899d52f
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2025-02-24 20:07:24 +0300

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,9 @@
+Merge branch 'boots_buttons'
+Merge remote-tracking branch 'origin/buttons' into boots_buttons
+Merge branches 'demo' and 'master' of https://github.com/YandexLyceum…
+Merge branch 'buttons' into demo
+fix readme
+dots
+Merge branch 'boots' into release
+heat added
+init
